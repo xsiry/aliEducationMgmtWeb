fcf8f5b11400c3d6bf7872377751b284c812e73c
xsiry <xsiries@gmail.com>
2017-06-20 18:34:05 +0800

    #调整

--- static/lv_mgmt/src/columns.js
+++ static/lv_mgmt/src/columns.js
@@ -9,28 +9,33 @@ define([{
   minWidth: 100,
   width: '15%'
 }, {
+  display: '点击地址',
+  name: 'clickurl',
+  minWidth: 120,
+  width: '15%'
+}, {
   display: '关注数',
   name: 'follow',
-  minWidth: 120,
+  minWidth: 80,
   width: '10%'
 }, {
   display: '排序',
   name: 'queue',
-  minWidth: 60,
-  width: '10%'
+  minWidth: 50,
+  width: '5%'
 }, {
   display: '是否显示角标',
   name: 'mark',
-  minWidth: 140,
-  width: '10%',
+  minWidth: 50,
+  width: '5%',
   render: function(rowdata, rowindex, value) {
     return value == 1 ? '是' : '否';
   }
 }, {
   display: '是否正在直播',
   name: 'live',
-  minWidth: 140,
-  width: '10%',
+  minWidth: 50,
+  width: '5%',
   render: function(rowdata, rowindex, value) {
     return value == 1 ? '是' : '否';
   }
